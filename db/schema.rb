0ada36ac125b5a7a60c4c430afee6b70a27e707f
Deborah Hamel <deborahleehamel@gmail.com>
2016-11-15 12:03:59 -0700

    add published to post and default unpublished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115185606) do
+ActiveRecord::Schema.define(version: 20161115190026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20161115185606) do
     t.string   "title"
     t.text     "body"
     t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.boolean  "published",    default: false
+    t.datetime "published_at"
   end
 
 end
